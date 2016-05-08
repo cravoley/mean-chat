3440dde8175e00199e9cd5a01b0aaab82117f8c2
Valdo Raya <cravoley@gmail.com>
2016-05-08 19:30:05 -0300

    add socket.io dependency

--- app/main.js
+++ app/main.js
@@ -3,3 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const socket = io("?name=Valdo&amp;city=Cidade");
+socket.on("connection",function(s){
+    clients.push(s);
+    console.log(s);
+})
+
+socket.on("message",function(data){
+    console.log(data);
+})
